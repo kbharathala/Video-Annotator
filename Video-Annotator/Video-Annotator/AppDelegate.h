eefcaa7471a5c934d664f99abd578a70093f9e1d
kbharathala <kbharathala@gmail.com>
2015-10-09 22:18:04 -0700

    adding ios project

---
+++ Video-Annotator/Video-Annotator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video-Annotator
+//
+//  Created by Krishna Bharathala on 10/9/15.
+//  Copyright (c) 2015 Krishna Bharathala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Video-Annotator/Video-Annotator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Video-Annotator
+//
+//  Created by Krishna Bharathala on 10/9/15.
+//  Copyright (c) 2015 Krishna Bharathala. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
